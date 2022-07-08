fe5b0c9a87289845e45bdcacec4330e2032fa911
arman9022 <arman.hub.bd@gmail.com>
2022-07-08 10:05:08 +0600

    a

---
+++ main.js
@@ -0,0 +1,45 @@
+function myFunction1() {
+    var element = document.getElementById("myDIV");
+    element.classList.add("mystyle1");
+ }
+ function leave1(){
+   var element = document.getElementById("myDIV");
+   element.classList.remove("mystyle1");
+ }
+
+ function myFunction2() {
+    var element = document.getElementById("myDIV");
+    element.classList.add("mystyle2");
+ }
+ function leave2(){
+   var element = document.getElementById("myDIV");
+   element.classList.remove("mystyle2");
+ }
+
+ function myFunction3() {
+    var element = document.getElementById("myDIV");
+    element.classList.add("mystyle3");
+ }
+ function leave3(){
+   var element = document.getElementById("myDIV");
+   element.classList.remove("mystyle3");
+ }
+
+ function myFunction4() {
+    var element = document.getElementById("myDIV");
+    element.classList.add("mystyle4");
+ }
+ function leave4(){
+   var element = document.getElementById("myDIV");
+   element.classList.remove("mystyle4");
+ }
+
+ function myFunction5() {
+    var element = document.getElementById("myDIV");
+    element.classList.add("mystyle5");
+ }
+ function leave5(){
+   var element = document.getElementById("myDIV");
+   element.classList.remove("mystyle5");
+ }
+
